1a6fb9fcd6f26b70dfa7bf25056b7cda8438f1bd
EduardoRomano77 <eduardo_romano@icloud.com>
2026-09-21 23:18:44 +0000

    ç

---
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/Categoria.java
@@ -0,0 +1,12 @@
+package br.edu.unifio.ecommerce;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Categoria {
+    private Short id;
+    private String nome;
+    private String descricao;
+}
